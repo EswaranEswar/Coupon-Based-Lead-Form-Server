7f3f684aad2bd01198c44cfec2b342168f1bd1d7
EswaranEswar <eswar9659@gmail.com>
2026-05-07 14:36:43 +0530

    implement NestJS application bootstrap and add Swagger API documentation support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import * as dns from 'node:dns';
 import { AppModule } from './app.module';
-
 import { ZodValidationPipe } from 'nestjs-zod';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { DEFAULT_CORS_ORIGIN, DEFAULT_PORT } from './shared/constants/constants';
 
 async function bootstrap() {
@@ -21,9 +20,22 @@ async function bootstrap() {
     new ZodValidationPipe(),
   );
 
- const port = Number(process.env.PORT) || DEFAULT_PORT;
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Coupon-Based Lead Form API')
+    .setDescription('API documentation for managing coupons and lead submissions')
+    .setVersion('1.0')
+    .addTag('coupons')
+    .addTag('leads')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = Number(process.env.PORT) || DEFAULT_PORT;
   await app.listen(port);
   console.log(`API running on http://localhost:${port}/`);
+  console.log(`Swagger Docs available on http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
